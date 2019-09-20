bd16bf36cd2ee992f71689f9e9a681cff146e575
Daniel Seifert <ds@shopmodule.com>
2019-09-20 16:05:36 +0200

    adjust documentation

--- example/simple.php
+++ example/simple.php
@@ -2,9 +2,18 @@
 
 require __DIR__ . '/../../../../source/bootstrap.php';
 
-\StartSQLLog();
+\D3StartSQLLog('Query for 100 items cheaper than 49,99');
 
 $db = \OxidEsales\Eshop\Core\DatabaseProvider::getDb(\OxidEsales\Eshop\Core\DatabaseProvider::FETCH_MODE_ASSOC);
 $list = $db->getAll('SELECT * FROM oxarticles WHERE oxprice < ? LIMIT 100', [49.99]);
 
-\StopSQLLog();
+\D3StopSQLLog();
+
+// or
+
+\D3StartSQLLog();
+
+$db = \OxidEsales\Eshop\Core\DatabaseProvider::getDb(\OxidEsales\Eshop\Core\DatabaseProvider::FETCH_MODE_ASSOC);
+$list = $db->getAll('SELECT * FROM oxarticles WHERE oxprice < ? LIMIT 100', [49.99]);
+
+\D3StopSQLLog();
